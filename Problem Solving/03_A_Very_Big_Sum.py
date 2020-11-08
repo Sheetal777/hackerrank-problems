3fd1dec434aab6b69d2235901304e000fb27f882
Sheetal <pandeysheetal777@gmail.com>
2020-11-08 19:55:21 +0530

    Create 03_A_Very_Big_Sum.py

---
+++ Problem Solving/03_A_Very_Big_Sum.py
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+    List<Integer> result = new ArrayList<>();
+        int alice = 0;
+        int bob = 0; 
+        for(int j=0;j<3;j++){
+         int[] score = new int[2];
+          score[0] = a.get(j);
+          score[1] = b.get(j);
+             if(score[0]<score[1])
+                alice++;
+                else if(score[1]<score[0])
+                bob++;       
+        }
+
+        result.add(0,bob);
+        result.add(1,alice);
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
