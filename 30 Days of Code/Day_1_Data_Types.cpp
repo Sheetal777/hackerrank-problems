3405f221a4230edf6718d2d93265be4812d53968
Sheetal <pandeysheetal777@gmail.com>
2020-11-11 08:01:03 +0530

    Create Day_1_Data_Types.cpp

---
+++ 30 Days of Code/Day_1_Data_Types.cpp
@@ -0,0 +1,20 @@
+    int i2;
+    double d2;
+    string s2;
+    
+    cin >> i2;
+    cin >> d2;
+    getline(cin >> ws, s2);
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    cout << i+i2 << endl;
+    cout << fixed << setprecision(1) << d+d2 << endl;
+    
+    // Print the sum of both integer variables on a new line.
+    cout << s+s2 << endl;
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
